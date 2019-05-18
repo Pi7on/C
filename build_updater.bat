86b0007cf1a0fdb42e935bb631fb9054c222d1d1
Pi7on <45372614+Pi7on@users.noreply.github.com>
2019-05-18 14:15:29 +0200

    Update build_updater.bat

--- build_updater.bat
+++ build_updater.bat
@@ -1,16 +1,10 @@
 @echo off
-
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/Pi7on/C/master/build.bat -OutFile build_new.bat"
-
 if exist "build_new.bat" (
 	echo Downlaod succesful!
 	del build.bat
 	ren build_new.bat build.bat
 	goto:END
 )
-
-
 echo download failed!
-
-
 :END
