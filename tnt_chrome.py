6898c123194c9398dcee1f11cf7ecadfa2f34619
Pi7on <45372614+Pi7on@users.noreply.github.com>
2019-07-31 03:15:21 +0200

    add args

--- tnt_chrome.py
+++ tnt_chrome.py
@@ -2,8 +2,17 @@ from selenium import webdriver as wd
 from selenium.webdriver.common.keys import Keys as k
 from selenium.webdriver.chrome.options import Options
 import os
+import sys
 import codecs
 import time
+
+#parse args----------------------------------------------------------------------
+if str(sys.argv[1]) == "--start" :
+	start = int(sys.argv[2])
+	
+if str(sys.argv[3]) == "--end" :
+	end = int(sys.argv[4])
+#-------------------------------------------------------------------------------
 
 #to save html page
 completeName = os.path.join('C:\\Users\\Marco\\Desktop\\selenium_proj', 'page.html')
@@ -17,7 +26,7 @@ file_object.write(driver.page_source)
 
 
 #da 2 a 9
-for current_page in range(2, 12):
+for current_page in range(start, end+1):
 	gotopage = driver.find_element_by_xpath('//*[@id="form_go_to_page"]/input[1]')
 	gotopage.send_keys(current_page)
 	search = driver.find_element_by_xpath('//*[@id="go_btn"]')
